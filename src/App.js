b1854a47b59cec247f69d564900f05534daec5a5
Your Name <you@example.com>
2024-05-30 04:32:24 +0700

    Mô tả thay đổi

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import './App.css';
-import Button from './components/button/index.';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from './routes';
+import { DefaultLayout } from './components/Layout';
+import { Fragment } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <Button />
-      <div style={{ padding: '0 32px' }}>
-        <h1 className="heading">CSS</h1>
-      </div>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+
+                        let Layout = DefaultLayout;
+
+                        if (route.layout === null) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
